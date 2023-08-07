3d10eacc03bfd08c29b1f26d386a04ec6b844238
imvake <104240205+imvake@users.noreply.github.com>
2023-08-07 19:07:42 +0400

    Add files via upload

---
+++ studying2/main.js
@@ -0,0 +1,239 @@
+// var char = '*';
+// var no1 = 5;
+// var no2 = 20;
+
+// switch(char){
+//     case "+":
+//     console.log(no1 + no2);
+//     break;
+//     case "-":
+//     console.log(no1 - no2);
+//     break;
+//     case "%":
+//         console.log(no1 % no2);
+//     break;
+//     case "*":
+//         console.log(no1 * no2);
+//     break;
+//     default:
+//         console.log("Wrong Char")
+// }
+
+// var x = 5; 
+// console.log(x++)
+// console.log(++x)
+// console.log(x++)
+// console.log(x++)
+// console.log(++x)
+// console.log(x)
+
+// var counter = 1;
+
+// while (counter <= 50)
+// {console.log(counter);
+//     counter ++
+// }
+
+
+
+// do{console.log(counter);
+//     counter ++
+// }
+// while (counter <= 50)
+
+
+
+// var numbers = [1,2,3,4,5,6,7,8,9,10];
+// var counter = 0
+// while(counter <= numbers.length){
+//     console.log(numbers[counter])
+//     counter++    
+// }
+
+// for(var i = 1;x<=20;x++ )
+// {
+//     console.log(i);
+// }
+
+// var names = ["Khalid","Ahmed","Alwaleed","Kareem"];
+
+// for(i = 0; i < names.length; i++ ){
+//     console.log(names[i])
+// }
+
+
+// var names = ["Khalid","Ahmed","Alwaleed","Kareem"];
+
+// for(i = 3; i > names.length; i-- ){
+//     console.log(names[i])
+    
+// }
+
+// var number1 = 100;
+// var number2 = 200;
+
+// function calcsum(){
+//     console.log(number1 + number2);
+// }
+
+
+// var x = 50;
+// var y = 80;
+
+// function getTotal(){
+//     return x + y
+// }
+
+// console.log(getTotal());
+
+
+// function sayHello(x){
+//     console.log(x);
+// }
+// sayHello("me")
+
+
+// function getSum(no1,no2){
+//     return no1 + no2
+// }
+
+// console.log(getSum(10+20))
+// console.log(getSum(100+200))
+// console.log(getSum(123+321))
+
+// function Calc(no1,no2,char){
+//     switch(char){
+//     case "+":
+//     return(no1 + no2);
+//     break;
+//     case "-":
+//     return(no1 - no2);
+//     break;
+//     case "%":
+//         return(no1 % no2);
+//     break;
+//     case "*":
+//         return(no1 * no2);
+//     break;
+//     default:
+//         return("Wrong Char")
+// }
+// }
+
+// console.log(Calc(2,3,"-"));
+
+// function validateNo(n0){
+//     if(n0 % 3 == 0 && n0 % 4 == 0)
+//     return "yes"
+//     else return "no"
+    
+// }
+
+// console.log(validateNo(4))
+
+// function greater(no1, no2)
+// {
+//     if(no1 > no2)
+//     return "First Number greater"
+// else if (no2 > no1){
+//     return "Second Number greater"
+    
+// }
+// }
+
+// console.log(greater(11,10))
+
+
+
+
+
+
+
+
+// function neg(no1)
+// {
+//     if(no1 >= 0)
+//     return "The number is greater than 0"
+//     else if(no1 <= 0)
+//     return "The number is les than 0"
+//     else return "Number is 0"
+// }
+// console.log(neg(0))
+
+// function maxMin(no1,no2,no3){
+//     if(no1 > no2 && no2 > no3){
+//         console.log(no1+" max" +)
+//     }
+//     else if(no2 > no1 ){
+//     console.log(no2+" max")
+//     console.log(no1+" min")}
+//     else if (no3 > no1){
+//         console.log(no3+" max")
+//         console.log(no1+" min")
+//     }
+//     else if (no3 > no2){
+//         console.log(no3+" max")
+//         console.log(no2+" min")
+//     }
+//     else{
+//         console.log(no3+" max")
+//         console.log(no2+" min")
+//     }
+// }
+
+// maxMin(5,2,6)
+
+// function evenOdd(no1){
+//     if(no1 % 2 === 0){
+//         console.log("even Number "+no1)
+
+//     }
+//     else{
+//         console.log("Odd Number "+no1)
+//     }
+// }
+
+// evenOdd(13)
+
+
+
+// names = "Alwaleed";
+// vowls = ['a','e','i','o','u']
+
+
+
+// for(i = names.length;vowls.length > 0 ; i--){
+//     if(names[i] == vowls[i])
+//     console.log(vowls[i])
+// }
+
+
+// function deleteNo(no){
+//     for(i=0; no >0;i++){
+//         console.log(i)
+//     }
+// }
+
+// deleteNo(3)
+
+// function Multi(no){
+//     for(i=1; i<=12; i++)
+//     {
+//         console.log(no * i)
+//     }
+
+// }
+// Multi(2)
+
+
+// function Eve(no)
+// {
+//     for(i=1; i <= no; i++){
+//         if(i % 2 === 0){
+//             console.log(i)
+//         }
+//     }
+// }
+
+
+// Eve(28);
